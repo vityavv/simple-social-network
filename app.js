6b5a8979aad19732b054f55983abfa84374dac58
Victor Veytsman <vityavv@gmail.com>
2017-12-05 11:38:13 -0500

    Add the get and post and data but not the api

--- app.js
+++ app.js
@@ -10,27 +10,39 @@ function serverfunc(req, res) {
 				res.writeHead(404, "404 not found", {"Content-Type": "text/html"});
 				return res.end("404 Not Found");
 			}
-			res.write(data);
-			res.end();
+			res.end(data);
 		});
-	}
-	if (query.pathname == "/front.js") {
+	} else if (query.pathname == "/front.js") {
 		fs.readFile("./front.js", function(error, data) {
 			if (error) {
 				res.writeHead(404, "404 not found", {"Content-Type": "text/html"});
 				return res.end("404 Not Found");
 			}
-			res.write(data);
-			res.end();
+			res.end(data);
 		});
+	} else if (query.pathname == "/data.json") {
+		if (req.method == "GET") {
+			fs.readFile("./data.json", function(error, data) {
+				if (error) {
+					res.writeHead(404, "404 not found", {"Content-Type": "text/html"});
+					return res.end("404 Not Found");
+				}
+				res.end(data);
+			});
+		} else if (req.method == "POST") {
+			var requestBody = "";
+			req.on("data", function(data) {
+				requestBody += data;
+				if (requestBody.length > 1e7) {
+					res.writeHead(413, "Request Entity Too Large", {"Content-Type": "text/html"});
+					res.end("Request Entity Too Large, Bitch");
+				}
+			});
+			req.on("end", function() {
+				//TODO: Write API. requestBody is the data recieved.
+			});
+		}
 	}
-	console.log(query.pathname);
 }
 
 http.createServer(serverfunc).listen(8080);
-
-/*
- * JS CODE:
- fetch("http://localhost:8080", {method: "post", body: "hello"}).then(function(r){return r.text()}).then(function(r){console.log(r)});
- * This means: fetch the localhost with the post method, and post "hello"
- */
